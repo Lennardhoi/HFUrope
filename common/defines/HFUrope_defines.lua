7c2f95fe45ba06a95eae092c05663cbe63e42744
Edvin2002 <55025658+Edvin2002@users.noreply.github.com>
2020-04-17 13:55:54 +0200

    okay

--- common/defines/HFUrope_defines.lua
+++ common/defines/HFUrope_defines.lua
@@ -1,6 +1,8 @@
 --Only HFUrope specific defines no duplicates of other define files
 --zoom
 NDefines.NFrontend.CAMERA_MIN_HEIGHT = 40
+NDefines.NFrontend.CAMERA_MAX_HEIGHT = 2000
 NDefines.NGraphics.GMT_OFFSET = 1270 -- X position on map, of Greenwitch GMT+0 (see also in shader daynight.fxh)
 NDefines.NNavy.NAVAL_RANGE_TO_INGAME_DISTANCE = 0.10
+NDefines.NGraphics.CAMERA_ZOOM_SPEED = 30
 --------------------------------------------------------------
