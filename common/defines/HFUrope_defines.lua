c9deded3193ff0865bbdc49ea08a4276fca3a788
Edvin2002 <55025658+Edvin2002@users.noreply.github.com>
2020-04-27 15:30:17 +0200

    smaller units might not work if you bought unitpacks

--- common/defines/HFUrope_defines.lua
+++ common/defines/HFUrope_defines.lua
@@ -6,4 +6,16 @@ NDefines.NGraphics.GMT_OFFSET = 1270 -- X position on map, of Greenwitch GMT+0 (
 NDefines.NNavy.NAVAL_RANGE_TO_INGAME_DISTANCE = 0.10
 NDefines.NGraphics.CAMERA_ZOOM_SPEED = 30
 NDefines.NMilitary.LAND_SPEED_MODIFIER = 0.035
+NDefines.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE = 700--500
+NDefines.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE_STRATEGIC = 500--350
+NDefines.NGraphics.MAP_ICONS_GROUP_SPLIT_SELECTED_LIMIT = 12
+NDefines.NGraphics.MAPICON_GROUP_PASSES = 10 -- 20	
+NDefines.NGraphics.MAP_ICONS_STATE_HUGE = 100
+NDefines.NGraphics.MAP_ICONS_STRATEGIC_AREA_HUGE = 220
+NDefines.NGraphics.MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 350
+NDefines.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 180
+NDefines.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 90 
+NDefines.NGraphics.LAND_COMBAT_DISTANCE_CUTOFF = 1000
+NDefines.NGraphics.UNITS_DISTANCE_CUTOFF = 200
 --------------------------------------------------------------
+--scale = 0.5 #scale = 0.8
